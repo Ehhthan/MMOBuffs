0f9135b076323f101b769a791863e9bf7e37510c
Ethan <ethan@plexiate.com>
2021-10-12 17:38:17 -0400

    init

---
+++ src/main/java/com/ehhthan/mmostatuseffects/MMOStatusEffects.java
@@ -0,0 +1,17 @@
+package com.ehhthan.mmostatuseffects;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MMOStatusEffects extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
