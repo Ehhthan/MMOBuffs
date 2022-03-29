ad57beefc95722cf92d5e2e68c094eb319c01822
Ethan <ethan@plexiate.com>
2022-03-29 00:11:48 -0400

    Added -s flag.

--- src/main/java/com/ehhthan/mmobuffs/command/MMOBuffsCommand.java
+++ src/main/java/com/ehhthan/mmobuffs/command/MMOBuffsCommand.java
@@ -54,42 +54,42 @@ public class MMOBuffsCommand extends BaseCommand {
     @Subcommand("give|add")
     @CommandPermission("mmobuffs.give")
     @Description("Give an effect to a player.")
-    @CommandCompletion("@players @effects @range:1-9 * @range:1-9 *")
-    @Syntax("<player> <effect> <duration> [duration-modifier] [stacks] [stack-modifier]")
+    @CommandCompletion("@players @effects @range:1-9 * @range:1-9 * -s")
+    @Syntax("<player> <effect> <duration> [duration-modifier] [stacks] [stack-modifier] [-s]")
     public void onGiveCommand(CommandSender sender, EffectHolder holder, StatusEffect effect, Integer duration, @Default("SET") Modifier durationModifier,
-                              @Default("1") Integer stacks, @Default("KEEP") Modifier stackModifier) {
+                              @Default("1") Integer stacks, @Default("KEEP") Modifier stackModifier, @Default String silent) {
         ActiveStatusEffect activeEffect = ActiveStatusEffect.builder(effect).startDuration(duration).startStacks(stacks).build();
         holder.addEffect(activeEffect, durationModifier, stackModifier);
 
         TagResolver resolver = TagResolver.builder().resolvers(activeEffect.getResolver()).resolver(Placeholder.component("player", holder.getPlayer().displayName())).build();
         Component message = language.getMessage("give-effect", true, resolver);
 
-        if (message != null)
+        if (message != null && !silent.equalsIgnoreCase("-s"))
             sender.sendMessage(message);
     }
 
     @Subcommand("permanent|perm")
     @CommandPermission("mmobuffs.permanent")
     @Description("Give a permanent effect to a player.")
-    @CommandCompletion("@players @effects * @range:1-9 *")
-    @Syntax("<player> <effect> [modifier] [stacks]")
+    @CommandCompletion("@players @effects * @range:1-9 * -s")
+    @Syntax("<player> <effect> [duration-modifier] [stacks] [stack-modifier] [-s]")
     public void onPermanentCommand(CommandSender sender, EffectHolder holder, StatusEffect effect, @Default("REPLACE") Modifier durationModifier,
-                                   @Default("1") Integer stacks, @Default("KEEP") Modifier stackModifier) {
+                                   @Default("1") Integer stacks, @Default("KEEP") Modifier stackModifier, @Default String silent) {
         holder.addEffect(ActiveStatusEffect.builder(effect).permanent(true).startStacks(stacks).build(), durationModifier, stackModifier);
 
         TagResolver resolver = TagResolver.builder().resolvers(effect.getResolver()).resolver(Placeholder.component("player", holder.getPlayer().displayName())).build();
         Component message = language.getMessage("give-effect-permanent", true, resolver);
 
-        if (message != null)
+        if (message != null && !silent.equalsIgnoreCase("-s"))
             sender.sendMessage(message);
     }
 
     @Subcommand("clear|remove")
     @CommandPermission("mmobuffs.clear")
     @Description("Remove a single effect, all non permanent effects, or every effect from a player.")
-    @CommandCompletion("@players @effects|all|permanent")
-    @Syntax("<player> <effect|all|permanent>")
-    public void onClearCommand(CommandSender sender, EffectHolder holder, String choice) {
+    @CommandCompletion("@players @effects|all|permanent -s")
+    @Syntax("<player> <effect|all|permanent> [-s]")
+    public void onClearCommand(CommandSender sender, EffectHolder holder, String choice, @Default String silent) {
         Component message;
         TagResolver.Single resolver = Placeholder.component("player", holder.getPlayer().displayName());
 
@@ -113,16 +113,16 @@ public class MMOBuffsCommand extends BaseCommand {
                 message = language.getMessage("clear-effect", true, TagResolver.builder().resolver(resolver).resolver(holder.getEffect(key).getResolver()).build());
             }
         }
-        if (message != null)
+        if (message != null && !silent.equalsIgnoreCase("-s"))
             sender.sendMessage(message);
     }
 
     @Subcommand("time|duration")
     @CommandPermission("mmobuffs.time")
     @Description("Alter the duration of an effect.")
-    @CommandCompletion("@players @effects * @range:1-9")
-    @Syntax("<player> <effect> <set|add|subtract|multiply|divide> <duration>")
-    public void onTimeCommand(CommandSender sender, EffectHolder holder, StatusEffect effect, Operation operation, int duration) {
+    @CommandCompletion("@players @effects * @range:1-9 -s")
+    @Syntax("<player> <effect> <set|add|subtract|multiply|divide> <duration> [-s]")
+    public void onTimeCommand(CommandSender sender, EffectHolder holder, StatusEffect effect, Operation operation, int duration, @Default String silent) {
         if (holder.hasEffect(effect.getKey())) {
             ActiveStatusEffect activeEffect = holder.getEffect(effect.getKey());
             int newDuration;
@@ -143,7 +143,7 @@ public class MMOBuffsCommand extends BaseCommand {
 
             TagResolver resolver = TagResolver.builder().resolvers(activeEffect.getResolver()).resolver(Placeholder.component("player", holder.getPlayer().displayName())).build();
             Component message = language.getMessage("time-effect", true, resolver);
-            if (message != null)
+            if (message != null && !silent.equalsIgnoreCase("-s"))
                 sender.sendMessage(message);
         } else {
             throw new InvalidCommandArgument("Player does not have that effect.");
@@ -153,9 +153,9 @@ public class MMOBuffsCommand extends BaseCommand {
     @Subcommand("stack|stacks")
     @CommandPermission("mmobuffs.stack")
     @Description("Alter the stacks of an effect.")
-    @CommandCompletion("@players @effects * @range:1-9")
-    @Syntax("<player> <effect> <set|add|subtract|multiply|divide> <stacks>")
-    public void onStackCommand(CommandSender sender, EffectHolder holder, StatusEffect effect, Operation operation, int stacks) {
+    @CommandCompletion("@players @effects * @range:1-9 -s")
+    @Syntax("<player> <effect> <set|add|subtract|multiply|divide> <stacks> [-s]")
+    public void onStackCommand(CommandSender sender, EffectHolder holder, StatusEffect effect, Operation operation, int stacks, @Default String silent) {
         if (holder.hasEffect(effect.getKey())) {
             ActiveStatusEffect activeEffect = holder.getEffect(effect.getKey());
             int newStacks;
@@ -178,7 +178,7 @@ public class MMOBuffsCommand extends BaseCommand {
             TagResolver resolver = TagResolver.builder().resolvers(activeEffect.getResolver()).resolver(Placeholder.component("player", holder.getPlayer().displayName())).build();
 
             Component message = language.getMessage("stack-effect", true, resolver);
-            if (message != null)
+            if (message != null && !silent.equalsIgnoreCase("-s"))
                 sender.sendMessage(message);
         } else {
             throw new InvalidCommandArgument("Player does not have that effect.");
